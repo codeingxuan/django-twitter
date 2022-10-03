327b047a7ea6ffcf9356aeeeb0ac0ab397a1dbc9
vagrant <vagrant@vagrant.vm>
2022-10-03 06:22:32 +0000

    User Api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,10 +1,48 @@
-# 如何序列化数据，转换为json格式
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        # 中括号改成了大括号
-        fields = ('url', 'username', 'email')
+        fields = ('username', 'email')
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def validate(self, data):
+        # TODO<HOMEWORK> 增加验证 username 是不是只由给定的字符集合构成
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    # 序列器内部会调用这个方法然后作为.save()的返回，validated_data也是验证过的数据
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
