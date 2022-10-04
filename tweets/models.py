d9420450f0f4a7a0530f1c706ae8bdcf76da44cb
vagrant <vagrant@vagrant.vm>
2022-10-04 16:03:33 +0000

    Initial Tweet Model

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from utils.time_helpers import utc_now
 
 class Tweet(models.Model):
     # who post the tweet
@@ -12,3 +12,11 @@ class Tweet(models.Model):
     ) # Fixed Format of ForeignKey
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def hours_to_now(self):
+        return (utc_now() - self.created_at).seconds // 3600
+
+    def __str__(self):
+        # 这里是你执行 print(tweet instance) 的时候会显示的内容
+        return f'{self.created_at} {self.user}: {self.content}'
