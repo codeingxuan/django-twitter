8fa9d002df3c1d625187f1a587892ee9fa1b3220
vagrant <vagrant@vagrant.vm>
2022-10-06 16:04:24 +0000

    Newsfeeds model and admin

---
+++ newsfeeds/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from newsfeeds.models import NewsFeeds
+
+@admin.register(NewsFeeds)
+class NewsFeedAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tweet', 'created_at')
+    date_hierarchy = 'created_at'
